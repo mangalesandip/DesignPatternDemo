943031f289f3af725efcf50f67db2cfd031cd1dd
mangalesandip <sandip.m3@gmail.com>
2021-10-08 11:10:58 +0530

    Simple example to demonstrate singlotone

--- src/SingletonClassDemo.java
+++ src/SingletonClassDemo.java
@@ -1,8 +1,26 @@
 
+class Singletone{
+	private static Singletone _INSTANCE= null;
+	private Singletone() {
+		System.out.println("Object created...");
+	}
+	
+	public static Singletone getInstance() {
+		if(_INSTANCE == null) {
+			_INSTANCE = new Singletone();
+		}
+		return _INSTANCE;
+	}
+}
+
 public class SingletonClassDemo {
 
 	public static void main(String[] args) {
 		System.out.println("This is singleDemo...");
+		Singletone objX = Singletone.getInstance();
+		Singletone objY = Singletone.getInstance();
+		
+		System.out.println("objX =>"+ objX);
+		System.out.println("objY =>"+ objY);
 	}
-
 }
